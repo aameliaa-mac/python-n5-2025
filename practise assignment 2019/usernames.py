8f16b9118e6b11634447328825d310548154ec50
Amelia Rosenbaum-Macaulay <A.Rosenbaum-Macaulay@albynschool.org>
2025-12-01 11:45:00 +0000

    kirsty is away

---
+++ practise assignment 2019/usernames.py
@@ -0,0 +1,11 @@
+import random
+
+endings = ['ing','end','axe','gex','goh']
+
+number = input("how many usernames should be made? ")
+
+for counter in range (number):
+    three = input("first three letters of students name: ")
+    length = len(three)
+    if length 
+    end = random.randstr(endings)
