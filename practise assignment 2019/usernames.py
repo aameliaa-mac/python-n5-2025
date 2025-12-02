ac4d40366e833a17670e10c1a5a29cf201bf88b1
Amelia Rosenbaum-Macaulay <A.Rosenbaum-Macaulay@albynschool.org>
2025-12-02 09:39:31 +0000

    kirsty is back

--- practise assignment 2019/usernames.py
+++ practise assignment 2019/usernames.py
@@ -1,11 +1,23 @@
 import random
 
-endings = ['ing','end','axe','gex','goh']
+endings = [0,1,2,3,4]
 
-number = input("how many usernames should be made? ")
+endings[0] = 'ing'
+endings[1] = 'end'
+endings[2] = 'axe'
+endings[3] = 'gex'
+endings[4] = 'goh'
+
+number = int(input("how many usernames should be made? "))
 
 for counter in range (number):
     three = input("first three letters of students name: ")
     length = len(three)
-    if length 
-    end = random.randstr(endings)
+    if length != 3:
+        print("error. length of inputted name is incorrect.")
+        three = input("first three letters of students name: ")
+    #end = random.choice(endings)
+    end = random.randint(0,4)
+    print("new username is: ", three + endings[end])
+
+
